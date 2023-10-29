3457232e4a75d80fb7e821bd14e4218757f52f62
saraporri <saraporricino@yahoo.com>
2023-10-29 10:35:07 +0100

    Updated github link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ import "./styles.css";
 export default function Footer() {
   return (
     <footer>
-      <a href="https://github.com/saraporri/weatherapp" target="_blank">
+      <a
+        href="https://github.com/saraporri/react-weather"
+        target="_blank"
+        rel="noreferrer"
+      >
         Open-source code
       </a>
       , by Sara Porricino
